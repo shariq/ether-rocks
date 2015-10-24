211ef6af1fe51d23e3af55935fa738b2f4f923bb
SyedAJafri <syedjafri99@gmail.com>
2015-10-23 20:06:32 -0400

    Added additional check to only trade if we have enough money. Throws an exception since that state should never happen. Means something is seriously wrong. We're spending more than we have

--- backend/exchanger.py
+++ backend/exchanger.py
@@ -32,23 +32,29 @@ apiKey = "FB8EJWXG-L75UYGW7-E6U482VD-N5R87R0C"
 secret = "3f709d585cf4c64ac4544e74b8d78d616119e2e522783bfe8460468c7db2208d54d1560df6e16e3a960dd52283331790672670826add166d06d45a640c3ff957"
 poloniexAcc = poloniex(apiKey, secret)
 
-balances = poloniexAcc.returnCompleteBalances()
 
-#Get withdrawel and deposit history up to current time
-currTime = str(time.time())
+
 
 prehistory = {}
 
+#TODO ask shariq did you say to only process 1 deposit at a time?
 def processDeposit(deposit):
-	processingDeposits.set(generateDepositID(desposit), ProcessingStatus.processing)
 	rate = 10000000 #TODO change (get normal market rate or slightly below?)
+
 	ethAmount = deposit["amount"]
 
 	coinsToBuy = 1
 	orderNum = poloniexAcc.sell("ETH_BTC", rate, coinsToBuy)
+	
+	#Safety: Checking if I have enough ether to sell
+	#If this under then there is an error
+	balances = poloniexAcc.returnBalances()
+	ethBalance = balances["ETH"]
+	if(ethBalance < ethAmount):
+		raise error("Error: trying to sell more ether then we have this state should never be reached")
 
 	if orderNum:
-		processingDeposits.set(deposit, orderNum) # sets a to b
+		processingDeposits.set(generateDepositID(deposit), ProcessingStatus.processing)
 		return True
 	else:
 		print("Unable to sell order for some reason: " + deposit)
@@ -57,9 +63,10 @@ def processDeposit(deposit):
 
 #If any orders are still processing then check their status
 #If finished forward to corresponding address
-def checkProcessingOrders:
-	tradeHistory = poloniex.returnTradeHistory("ETH_BTC")
+def checkProcessingOrders():
+	tradeHistory = poloniexAcc.returnTradeHistory("ETH_BTC")
 	for trades in tradeHistory:
+		done = False #TODO see if trade is finished
 		if done:
 			processingDeposits.remove(generateDepositID(deposit))
 			processedDeposits.set(generateDepositID(deposit), True)
@@ -67,21 +74,25 @@ def checkProcessingOrders:
 #Generate a "unique" id by using a timestamp and amount
 #Note probably not actually unique
 def generateDepositID(deposit):
-	return "timestamp:" + deposit["timestamp"] + " amount:" + deposit["amount"] + deposit
+	return "timestamp:" + str(deposit["timestamp"]) + " amount:" + deposit["amount"]
 
 while True:
+
 	#TODO use a timed wait until depositHistory != previousHistory to avoid unneeded work
+
+	#Get withdrawel and deposit history up to current time
+	currTime = str(time.time())
 	#TODO consider setting start time to a month ago or something like that
 	depositHistory = poloniexAcc.returnDepositsWithdrawals({"start": 0, "end": currTime})["deposits"]
 	
 	#For deposits not already
 	for deposit in depositHistory:
-		depositId = generateDepositID()
+		depositId = generateDepositID(deposit)
 		#Some deposits are pending or whateves ignore them
 		if deposit["status"] == "COMPLETE" and deposit["currency"] == "ETH":
-			if not processingDeposits.get(generateDepositID(desposit)):
+			if not processingDeposits.get(generateDepositID(deposit)):
 				processDeposit(deposit)
 
 	checkProcessingOrders()
 	prehistory = history
-	sleep 1
+	sleep(1)
